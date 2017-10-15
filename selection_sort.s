5525b970aab9c81c82dc9f1c8a1a21c069e22bba
cybergartar <kamonnop.a@gmail.com>
2017-10-15 20:45:25 +0700

    [DEV] New version of asm selection sort
    Based on selection_sort_in_c.c

--- selection_sort.s
+++ selection_sort.s
@@ -1,55 +1,146 @@
-        AREA myData, DATA, READWRITE
+    AREA myData, DATA, READWRITE
+    ; an array to sort and it's length
 array   DCD 24, 10, 56, 62, 50, 43, 21, 17, 35, 55
 len		EQU 10
-	
-	AREA myCode, CODE
-		ENTRY
+
+    AREA myCode, CODE
+        ENTRY
         EXPORT main
             
 main
     NOP
-    ; store array address in r0
+    ; store array address in [r0]
     LDR		r0, =array
-    
-    ; init variable
-    MOV		r1, #len                ; array len (n)
-    SUB		r2, r1, #1              ; array len - 1 (n-1)
-    MOV		r3, #0                  ; i
-    MOV		r4, #0                  ; j
-    MOV		r5, #0                  ; min_idx
-    
-    NOP
-    
-loop_i
-    MOV		r5, r3                  ; min_idx = i
-    ADD		r4, r3, #1              ; j = i + 1
-    
-loop_j
-    LDR		r6, [r0, r4, LSL #2]    ; array[j]
-    LDR		r7, [r0, r5, LSL #2]    ; array[min_idx]
-    CMP		r6, r7                  ; if (array[j] < array[min_idx])
-    
-    BGE		finish_cmp
-    
-    MOV		r5, r4                  ; min_idx = j
 
-finish_cmp
-    ADD		r4, r4, #1              ; j++
-    CMP		r4, r1                  ; j < n?
-    BLT		loop_j                  ; if less, continue do loop_j
-    
-    LDR		r6, [r0, r5, LSL #2]    ; r6 = array[min_idx]
-    LDR		r7, [r0, r3, LSL #2]    ; r7 = array[i]
-    STR		r6, [r0, r3, LSL #2]    ; SWAP: array[i] = r6
-    STR		r7, [r0, r5, LSL #2]    ; array[min_idx] = r7
-    
-    ADD		r3, r3, #1              ; i++
-    CMP		r3, r2                  ; i < n-1 ?
-    BLT		loop_i                  ; if less, continue do loop_i
-    
+    ; let [r1] contains limit of array's index 
+    ; (this is lim variable in sort function)
+    MOV     r1, #len
+    SUB     r1, r1, #1
+
+    ; let r2 be 1st parameter of sort function
+    MOV     r2, #0
+
+    ; call sort function
+    BL      sort
     NOP
 
-stop	
-    B		stop                    ; finish selection sort
+stop
+    ; finish selection sort
+    B		stop
+
+;=================== END main() =================;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;               function sort()                  ;
+; do a selection sort from index 0 to index [r1] ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+sort
+    ; store return address and stage in stack
+    SUB     sp, sp, #8
+    STR     lr, [sp, #0]
+    STR     r2, [sp, #4]
+
+    ; if (stage == lim) then return
+    CMP     r2, r1
+    BEQ     ret_sort
+
+    ; call findmin with parameters
+    ; cmp = stage = [r2]
+
+    ; now + 1 = [r3]
+    ADD     r3, r2, #1
+    ; stop = [r4] = [r1] (9)
+    MOV     r4, r1
+    BL      find_min
+
+    ; store min_indx in [r3]
+    MOV     r3, r2
+    ; recall original [r2] (stage)
+    ; from stack
+    LDR     r2, [sp, #4]
+    ; swap array[min_indx] and array[stage]
+    ;              [r3]              [r2]
+    BL      swap
+
+    ; call sort(stage + 1, lim)
+    ; stage + 1 = [r2] + 1
+    ADD     r2, r2, #1
+    BL      sort
+
+    ; sort function's return
+ret_sort
+    ; recall lr back from stack
+    LDR     lr, [sp, #0]
+    ; free stack
+    ADD     sp, sp, #8
+    ; return
+    MOV     pc, lr
+;=================== END sort() =================;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                function find_min               ;
+; find an index of array that contains smallest  ;
+; number in range stage to lim                   ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+find_min
+    ; store return address in stack
+    SUB     sp, sp, #4
+    STR     lr, [sp, #0]
+
+    ; now = r3
+    ; cmp = r2
+    ; stop = r4
+
+    ; variable this_is_min will be stored in [r2]
+
+    ; if (array[now] < array[cmp])
+if_ary_now_less_than_ary_cmp
+    LDR     r5, [r0, r3, LSL #2]
+    LDR     r6, [r0, r2, LSL #2]
+    CMP     r5, r6
+    BGE     endif_ary_now_less_than_ary_cmp
+    ; this_is_min = now [r3]
+    MOV     r2, r3
+
+endif_ary_now_less_than_ary_cmp
+    ; if jump to this directly, 
+    ; this_is_min = cmp = [r2]
+    NOP
+
+    ; if (now == stop)
+if_now_equal_to_stop
+    CMP     r3, r4
+    ; then return
+    BEQ     ret_find_min
+
+    ; else call find_min with parameters
+    ; cmp = this_is_min = [r2]
+    ; now = now + 1 = [r3] + 1
+    ; stop = [r4]
+    ADD     r3, r3, #1
+    BL      find_min
+
+    ; find_min's function reuturn
+ret_find_min
+    ; recall lr back from stack
+    LDR     lr, [sp, #0]
+    ; free stack
+    ADD     sp, sp, #4
+    ; return
+    MOV     pc, lr
+;================ END find_min() ===============;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; function swap                                  ;
+; swap value in array index [r2] and [r3]        ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+swap
+    LDR     r4, [r0, r2, LSL #2]
+    LDR     r5, [r0, r3, LSL #2]
+    STR     r4, [r0, r3, LSL #2]
+    STR     r5, [r0, r2, LSL #2]
+    MOV     pc, lr
+;=================== END swap() =================;
 
     END
